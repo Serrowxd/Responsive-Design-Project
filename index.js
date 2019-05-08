2479fdccd01d4e4d9ce40b81a31c0dd1285fa732
Jared Cooper <jaredrcooper@protonmail.com>
2019-05-08 07:39:46 -0400

    added cookie modal, button is hiding the modal on click correctly

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const classScroll = () => {
     let nav = document.querySelector('.main-header');
     nav.className = ('main-header-scroll');
 }
+
+
+
+// modal-cookies
+
+// modal-backdrop
+
+// toggle hide class on button click
+
+
+let backdrop = document.querySelector('.modal-backdrop');
+let modal = document.querySelector('.modal-cookies');
+let acceptCookiesButton = document.getElementById('modal-cookies__button');
+
+
+acceptCookiesButton.addEventListener('click', function(){
+    console.log('working');
+    backdrop.classList.add('hide');
+    modal.classList.add('hide');
+})
