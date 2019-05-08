0e145436929a30b717bcaa2ba20c99a2a2248c17
Jared Cooper <jaredrcooper@protonmail.com>
2019-05-08 07:55:47 -0400

    modal and modal backdrop are now fixed when scrolling

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-window.onscroll = () => {    
-    classScroll();    
-}
-
-const classScroll = () => {
-    let nav = document.querySelector('.main-header');
-    nav.className = ('main-header-scroll');
-}
-
-
-
-// modal-cookies
-
-// modal-backdrop
-
-// toggle hide class on button click
-
+//defining variables
 
 let backdrop = document.querySelector('.modal-backdrop');
 let modal = document.querySelector('.modal-cookies');
 let acceptCookiesButton = document.getElementById('modal-cookies__button');
 
+window.onscroll = () => {    
+    classScroll();    
+}
+
+//event listeners
 
 acceptCookiesButton.addEventListener('click', function(){
     console.log('working');
     backdrop.classList.add('hide');
     modal.classList.add('hide');
 })
+
+//functions
+
+const classScroll = () => {
+    let nav = document.querySelector('.main-header');
+    nav.className = ('main-header-scroll');
+}
